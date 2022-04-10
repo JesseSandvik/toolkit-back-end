7984d8236464154d4c39b78d55190f1f720d569e
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-10 16:06:02 -0400

    register route basic tests are functional and pass

---
+++ __tests__/register.spec.js
@@ -0,0 +1,24 @@
+const app = require('../src/app');
+const request = require('supertest');
+
+describe('Register', () => {
+  const user = {
+    name: 'Harry Potter',
+    email: 'hpotter@hogwarts.com',
+    password: 'hpotter',
+  };
+  it('01. POST request is successful', async () => {
+    const res = await request(app)
+      .post('/register')
+      .send(user);
+    expect(res.status).toEqual(201);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+  });
+  it('02. Registered user data matches POST request', async () => {
+    const res = await await request(app)
+      .post('/register')
+      .send(user);
+    expect(res.body[0].name).toEqual(user.name);
+    expect(res.body[0].email).toEqual(user.email);
+  });
+});
--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -2,7 +2,7 @@ const server = require('../src/app');
 const request = require('supertest');
 
 describe('Server', () => {
-  it('01. Renders Hello World message', async () => {
+  it('01. Renders "Hello World!" message', async () => {
     const res = await request(server).get('/');
     expect(res.status).toEqual(200);
     expect(res.type).toEqual(expect.stringContaining('json'));
