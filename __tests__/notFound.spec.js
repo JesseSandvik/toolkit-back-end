68dc8201673e1a3c2e28cdc91c19a7bab932a6a1
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-11 17:49:02 -0400

    expanded testing for 404 route

--- __tests__/notFound.spec.js
+++ __tests__/notFound.spec.js
@@ -5,5 +5,7 @@ describe('Not Found', () => {
   it('01. Returns a 404 status and message for not found routes', async () => {
     const res = await request(app).get('/this-is-a-bad-route');
     expect(res.status).toEqual(404);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+    expect(res.body).toEqual({error: '404 Not Found'});
   });
 });
