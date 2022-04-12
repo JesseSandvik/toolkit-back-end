8812f0993fc43a1bcb2eee2507ceab9190c3e95a
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-11 22:10:55 -0400

    added additional unit tests for register route

--- src/register/register.spec.js
+++ src/register/register.spec.js
@@ -2,19 +2,25 @@ const app = require('../app');
 const request = require('supertest');
 
 describe('Register', () => {
-  const user = {
-    name: 'Harry Potter',
-    email: 'hpotter@hogwarts.com',
-    password: 'hpotter',
-  };
   it('01. POST request is successful', async () => {
+    const user = {
+      name: 'Harry Potter',
+      email: 'hpotter@hogwarts.com',
+      password: 'hpotter',
+    };
     const res = await request(app)
       .post('/register')
       .send({data: user});
+    expect(res.body.error).toBeUndefined();
     expect(res.status).toEqual(201);
     expect(res.type).toEqual(expect.stringContaining('json'));
   });
   it('02. Registered user data matches POST request', async () => {
+    const user = {
+      name: 'Harry Potter',
+      email: 'hpotter@hogwarts.com',
+      password: 'hpotter',
+    };
     const res = await request(app)
       .post('/register')
       .send({data: user});
@@ -27,6 +33,88 @@ describe('Register', () => {
       .post('/register')
       .set('Accept', 'application/json')
       .send({data: {}});
+    expect(res.body.error).toBeDefined();
+    expect(res.status).toBe(400);
+  });
+  it('04. Returns 400 if name is missing', async () => {
+    const user = {
+      email: 'hpotter@hogwarts.com',
+      password: 'hpotter@@',
+    };
+    const res = await request(app)
+      .post('/register')
+      .set('Accept', 'application/json')
+      .send({data: user});
+    expect(res.body.error).toBeDefined();
+    expect(res.body.error).toContain('name');
+    expect(res.status).toBe(400);
+  });
+  it('05. Returns 400 if name is empty', async () => {
+    const user = {
+      name: '',
+      email: 'hpotter@hogwarts.com',
+      password: 'hpotter@@',
+    };
+    const res = await request(app)
+      .post('/register')
+      .set('Accept', 'application/json')
+      .send({data: user});
+    expect(res.body.error).toBeDefined();
+    expect(res.body.error).toContain('name');
+    expect(res.status).toBe(400);
+  });
+  it('06. Returns 400 if email is missing', async () => {
+    const user = {
+      name: 'Harry Potter',
+      password: 'hpotter@@',
+    };
+    const res = await request(app)
+      .post('/register')
+      .set('Accept', 'application/json')
+      .send({data: user});
+    expect(res.body.error).toBeDefined();
+    expect(res.body.error).toContain('email');
+    expect(res.status).toBe(400);
+  });
+  it('07. Returns 400 if email is empty', async () => {
+    const user = {
+      name: 'Harry Potter',
+      email: '',
+      password: 'hpotter@@',
+    };
+    const res = await request(app)
+      .post('/register')
+      .set('Accept', 'application/json')
+      .send({data: user});
+    expect(res.body.error).toBeDefined();
+    expect(res.body.error).toContain('email');
+    expect(res.status).toBe(400);
+  });
+  it('08. Returns 400 if password is missing', async () => {
+    const user = {
+      name: 'Harry Potter',
+      email: 'hpotter@hogwarts.com',
+    };
+    const res = await request(app)
+      .post('/register')
+      .set('Accept', 'application/json')
+      .send({data: user});
+    expect(res.body.error).toBeDefined();
+    expect(res.body.error).toContain('password');
+    expect(res.status).toBe(400);
+  });
+  it('09. Returns 400 if password is empty', async () => {
+    const user = {
+      name: 'Harry Potter',
+      email: 'hpotter@hogwarts.com',
+      password: '',
+    };
+    const res = await request(app)
+      .post('/register')
+      .set('Accept', 'application/json')
+      .send({data: user});
+    expect(res.body.error).toBeDefined();
+    expect(res.body.error).toContain('password');
     expect(res.status).toBe(400);
   });
 });
