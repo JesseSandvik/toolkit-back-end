3d60a5054bca5bdc8310aeb6cdfda1a66277d614
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-16 02:12:41 -0400

    test added and passing for commitments 404 bad route

--- src/commitments/commitments.spec.js
+++ src/commitments/commitments.spec.js
@@ -8,4 +8,9 @@ describe('Commitments', () => {
         expect(res.status).toEqual(200);
         expect(res.type).toEqual(expect.stringContaining('json'));
     });
+    it('02. Returns a 404 for a missing route', async () => {
+        const res = await request(app).get('/commitments/bad-route').set('Accept', 'application/json');
+        expect(res.status).toEqual(404);
+        expect(res.body.error).toEqual('Path not found: /commitments/bad-route');
+    });
 });
--- src/errors/notFound/notFound.spec.js
+++ src/errors/notFound/notFound.spec.js
@@ -6,7 +6,7 @@ describe('Not Found', () => {
     const res = await request(app)
       .get('/this-is-a-bad-route')
       .set('Accept', 'application/json');
-    expect(res.status).toBe(404);
-    expect(res.body.error).toBe('Path not found: /this-is-a-bad-route');
+    expect(res.status).toEqual(404);
+    expect(res.body.error).toEqual('Path not found: /this-is-a-bad-route');
   });
 });
