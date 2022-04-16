676f30f4d899334f52df33bc5dbb28edc3758260
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-16 15:23:56 -0400

    test passes for bad route, requests

--- src/requests/requests.spec.js
+++ src/requests/requests.spec.js
@@ -4,6 +4,12 @@ const app = require('../app');
 describe('Requests', () => {
     it('01. Returns a 200 status for a successful GET request', async () => {
         const res = await request(app).get('/requests').set('Accept', "application/json");
+        expect(res.body.error).toBeUndefined();
         expect(res.status).toEqual(200);
+    });
+    it('02. Returns a 404 status for a non-existent route', async () => {
+        const res = await request(app).get('/requests/bad-route').set('Accept', 'application/json');
+        expect(res.status).toEqual(404);
+        expect(res.body.error).toContain('/requests/bad-route');
     });
 });
