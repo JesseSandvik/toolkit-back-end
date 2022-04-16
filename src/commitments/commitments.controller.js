52b2f4eab355c9b9d4657fd2471531159b64d64f
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-16 15:01:46 -0400

    updated data for toolkit

--- src/commitments/commitments.controller.js
+++
@@ -1,9 +0,0 @@
-const commitments = require('../db/commitments.json');
-
-const listCommitments = async (req, res) => {
-    res.status(200).json({ data: commitments});
-}
-
-module.exports = {
-    list: listCommitments,
-}
