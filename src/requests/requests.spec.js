ea5881dab523868bd02ee84124c9a6772a45d72e
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-18 21:44:22 -0400

    first test for requests POST created

--- src/requests/requests.spec.js
+++ src/requests/requests.spec.js
@@ -7,9 +7,13 @@ describe('Requests', () => {
         expect(res.body.error).toBeUndefined();
         expect(res.status).toEqual(200);
     });
-    it('02. Returns a 404 status for a non-existent route', async () => {
+    it('02. Returns a 404 status for a GET request to a non-existent route', async () => {
         const res = await request(app).get('/requests/bad-route').set('Accept', 'application/json');
         expect(res.status).toEqual(404);
         expect(res.body.error).toContain('/requests/bad-route');
+    });
+    it('03. Returns a 400 if the name property is missing', async () => {
+        const res = await request(app).get('/requests').set('Accept', 'application/json');
+        expect(res.status).toEqual(400);
     });
 });
