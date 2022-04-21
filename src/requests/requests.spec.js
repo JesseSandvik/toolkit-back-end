4b9d83983851585f8d6a761bfeb152dd63e8dd0c
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-20 20:17:01 -0400

    added additional tests for POST request to requests

--- src/requests/requests.spec.js
+++ src/requests/requests.spec.js
@@ -22,4 +22,9 @@ describe('Requests', () => {
         expect(res.body.error).toBeDefined();
         expect(res.status).toEqual(400);
     });
+    it('05. Returns a 400 if the name property is an empty string', async () => {
+        const res = await request(app).post('/requests/new').set('Accept', 'application/json');
+        expect(res.body.error).toBeDefined();
+        expect(res.status).toEqual(400);
+    });
 });
